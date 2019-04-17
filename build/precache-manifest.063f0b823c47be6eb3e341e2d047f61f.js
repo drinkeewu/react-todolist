dd4a8d9eca515cffc458dc0d89291b989db4bdf2
drinkeewu <773119690@qq.com>
2019-04-17 10:06:02 +0800

    chore: build project

---
+++ build/precache-manifest.063f0b823c47be6eb3e341e2d047f61f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "509ff71560ccd6848ea8",
+    "url": "/static/js/main.270e00f1.chunk.js"
+  },
+  {
+    "revision": "01f644225ed79bc6ff6c",
+    "url": "/static/js/2.5520bfd9.chunk.js"
+  },
+  {
+    "revision": "509ff71560ccd6848ea8",
+    "url": "/static/css/main.38f53001.chunk.css"
+  },
+  {
+    "revision": "c05daf53b92f03df0068d4a077114556",
+    "url": "/index.html"
+  }
+];
